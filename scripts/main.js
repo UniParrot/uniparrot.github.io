7a44274493fbc63a2ef95327110a32225fea76e2
intlparrot@gmail.com <intlparrot@gmail.com>
2022-11-20 15:16:35 +0900

    delete

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,1 @@
 const html = document.querySelector('html');
-
-const button = document.createElement('button');
-button.setAttribute('class', 'mainButton');
-button.innerText = "CLICKME";
-html.appendChild(button);
-
-button.onclick = function () {
-  alert("hi");
-}
