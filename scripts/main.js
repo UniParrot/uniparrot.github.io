506162270d82f17ada8233a84a18d5ffc82f65ff
UniParrot <57437216+UniParrot@users.noreply.github.com>
2023-02-09 12:33:46 +0900

    webgl test

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
 const html = document.querySelector('html');
+var canvas = document.querySelector("#c");
+var gl = canvas.getContext("webgl");
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+    gl.deleteShader(shader);
+}
